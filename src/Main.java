e7ed2e73b8f9810167be9441e0928f79ff8f4156
RifatNPC <rifatakmalsetianto@gmail.com>
2025-10-20 10:04:32 +0700

    s1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Program Java Sederhana ===");
+
+        // Deklarasi variabel
+        String nama = "Rifat Akmal Setianto";
+        int nim = 494;
+        String kelas = "IMK 3K";
+
+        // Menampilkan output
+        System.out.println("Nama  : " + nama);
+        System.out.println("NIM   : " + nim);
+        System.out.println("Kelas : " + kelas);
+
+        // Operasi sederhana
+        int angka1 = 10;
+        int angka2 = 5;
+        int hasil = angka1 + angka2;
+
+        System.out.println("\nHasil penjumlahan " + angka1 + " + " + angka2 + " = " + hasil);
+        System.out.println("Program selesai!");
+    }
+}
